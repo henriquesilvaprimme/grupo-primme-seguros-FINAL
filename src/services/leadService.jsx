e42ad417cb1e890492cb597c3419962c31c97cb2
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-06-04 13:36:24 -0300

    Update leadService.jsx

--- src/services/leadService.jsx
+++ src/services/leadService.jsx
@@ -1,6 +1,6 @@
 // src/services/leadService.js
 export async function buscarLeadsNaoAtribuidos() {
-  const url = 'https://script.google.com/macros/s/SEU_DEPLOY_ID/exec'; // Substitua SEU_DEPLOY_ID pelo seu ID de deploy
+  const url = 'https://script.google.com/macros/s/AKfycbxueF8pDseB_ypxctET-IqgUQijcGXyFoOSJb-0cVfMJFDbyht8qHVnEYdVAK-pt-aWzQ/exec'; // Substitua SEU_DEPLOY_ID pelo seu ID de deploy
   const params = new URLSearchParams({ acao: 'listarLeadsNaoAtribuidos' });
 
   try {
